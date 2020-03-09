55eb42a6b04f01726951e03f0c00bbb868d64173
Gnglas <2254228017@qq.com>
2020-03-09 17:47:47 +0800

    my linux

---
+++ centos_install.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+echo "前言"
+echo "提示：这个脚本是我在manjaor上写的，所用的桌面环境是i3，所以如果不需要配置i3请把不相关的命令注释掉谢谢！"
+echo "如果你没有意见请忽略！"
+echo "----------------------------------------Gnglas"
+#下载i3桌面环境
+sudo yum install i3
+#i3wm 的一些截图工具
+sudo yum install peek xclip scrot
+#下载一些美化的插件
+sudo yum install screenfetch
+mkdir ~/.config/i3/screencut/ ~/.vim/ 
+#自动配置vim
+curl -sLf https://spacevim.org/cn/install.sh | bash -s -- --install vim
+#自动配置vim的配色
+git clon git@github.com:denstiny/Gnglas.git ~/Gnglas
+mv Gnglas/Image/ ~/
+mv Gnglas/my_i3/ .config/i3/
+mv Gnglas/vimrc ~/.vim/.vintrc
+#自动配置manjaror软件源需要就取消注释
+#mv Gnglas/pacman.conf Gnglas/mirrorlist  /etc/
+bash .del.sh
+
